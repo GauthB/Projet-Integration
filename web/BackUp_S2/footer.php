abc646974521e851d77ac7ca19aaec3d9ae5cce6
Bohyn Gauthier <39745082+GauthB@users.noreply.github.com>
2019-09-30 18:06:21 +0200

    Add files via upload

---
+++ web/BackUp_S2/footer.php
@@ -0,0 +1,35 @@
+<div class="container">
+    <div class="row mb-5">
+        <div class="col-md-4">
+            <h2 class="footer-heading text-uppercase mb-4">A propos</h2>
+            <p>Ceci est une invention dû à notre projet d'intégration.</p>
+        </div>
+        <div class="col-md-3 ml-auto">
+            <h2 class="footer-heading text-uppercase mb-4">Liens</h2>
+            <ul class="list-unstyled">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="info.php">A propos</a></li>
+                <li><a href="equipe.php">Groupe</a></li>
+                <li><a href="lieux.php">Lieu</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+        <div class="col-md-4">
+            <h2 class="footer-heading text-uppercase mb-4">Contactez-nous</h2>
+            <p>
+                <a href="mailto:peopleflux@gmail.com" class="p-2 pl-0"><span> People Flux </span></a>
+
+            </p>
+        </div>
+    </div>
+    <div class="row">
+
+        <div class="col-md-12 text-center">
+            <div class="border-top pt-5">
+                <p>
+                    &copy; <script>document.write(new Date().getFullYear());</script> Team People Flux</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
