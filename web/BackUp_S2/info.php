39ab95a8802dfbe6c4a35a5d9ac639a05c3311c0
Bohyn Gauthier <39745082+GauthB@users.noreply.github.com>
2019-09-30 18:08:16 +0200

    Add files via upload

---
+++ web/BackUp_S2/info.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Workshop &mdash; Colorlib Website Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="site-wrap">
+
+    <div class="site-mobile-menu">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+    </div>
+
+    <header class="site-navbar py-3" role="banner">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-11 col-xl-2">
+                    <h1 class="mb-0"><a href="index.html" class="text-white h2 mb-0">People<span class="text-primary">Flux</span> </a></h1>
+                </div>
+                <div class="col-12 col-md-10 d-none d-xl-block">
+                    <nav class="site-navigation position-relative text-right" role="navigation">
+                        <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
+                            <li><a href="index.php">Accueil</a></li>
+                            <li class="active"><a href="info.php">A propos</a></li>
+                            <li><a href="equipe.php">Groupe</a></li>
+                            <li><a href="lieux.php">Lieux</a></li>
+                            <li><a href="contact.html">Contacts</a></li>
+                            <li class="cta"><a href="buy.php">Commander</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+            </div>
+        </div>
+
+    </header>
+
+    <div class="site-section site-hero inner">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-10">
+                    <span class="d-block mb-3 caption" data-aos="fade-up">À savoir!</span>
+                    <h1 class="d-block mb-4" data-aos="fade-up" data-aos-delay="100">À propos de nous</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-section">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-lg-4 ">
+                    <div class="site-section-heading" data-aos="fade-up" data-aos-delay="100">
+                        <h2>Étudiants</h2>
+                    </div>
+                </div>
+                <div class="col-lg-5 mt-5 pl-lg-5" data-aos="fade-up" data-aos-delay="200">
+                    <p>Nous sommes 7 amis - étudiant à l'EPHEC de Louvain-la-Neuve en 3ème Technologie de l'Informatique.</p>
+                </div>
+            </div>
+            <div class="row mb-5">
+                <div class="col-12" data-aos="fade-up" data-aos-delay="300">
+                    <img src="images/hero_1.jpg" alt="Image" class="img-fluid">
+                </div>
+            </div>
+
+            <div class="col-md-4" data-aos="fade-up" data-aos-delay="400"><span class="text-primary">L'histoire ...</span></div>
+            <div  data-aos="fade-up" data-aos-delay="500">
+
+                <p>Tout est parti d’un projet à réaliser à 7!</br>
+                    Pour commencer, nos idées partaient de gauche à droite, … dans tous les sens!</br>
+                    Après plusieurs heures de réflexion, nous nous sommes rendu compte d’un point commun qui pouvait fort nous ennuyer et effectivement les autres étudiants de notre âge également!..</br>
+                    Le nombre de participant arrivant au lieu de la scène!</br>
+                    De cette idée-là, nous nous sommes dit qu’il fallait trouver une idée pour pouvoir connaitre le VRAI nombre de participant en temps DIRECT au lieu souhaité!</p>
+
+                <p>L’idée est venu de faire un site ou même une app qui pouvait nous montrer tout cela en direct! Plus besoin de se déplacer ou quoi que ce soit pour voir le monde sur place!</p>
+
+                <p> Nous avons donc eu l’idée de créer un mécanisme avec un laser qui serait capable de compter le nombre de personnes présentent sur place.</p>
+
+                <p>  Nous allons utiliser 2 lasers qu’on placera à la rentrée et également à la sortie pour tout calculer. Cela n’est pas tout! Nous allons lier cette puce intelligente à un
+                    serveur qui transmettra toutes les données au site web et également à l’app que vous aurez bien évidemment installé auparavant sur votre GSM!</p>
+
+                <p>  Pour mieux comprendre? N’hésitez à installer l’app ou regarder dans notre onglet lieu s’il y a des évènements en ce moment!</p>
+
+                <p>  Pas d’évènement? Nous allons également installer ce prototype dans certaines écoles pour apercevoir le nombre de participant dans une classe par exemple!</p>
+
+                <p>  N’hésitez pas à nous contacter si vous avez des questions ou quoi que ce soit!</p>
+
+                <p>  Intéressé par le projet? Vous voulez tester cette installation dans votre école? Votre évènement? Votre soirée? Contactez-nous!!</p>
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <
+
+    <div class="site-section">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-lg-4 ">
+                    <div class="site-section-heading" data-aos="fade-up">
+                        <h2>Outils</h2>
+                    </div>
+                </div>
+                <div class="col-lg-6 mt-5 pl-lg-5" data-aos="fade-up" data-aos-delay="100">
+                    <p>Voici ci-dessous tous les outils, que ça soit programmation, electronique, .. que nous avons utilisé durant le projet d'intégration.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
+                    <img src="images/logo_1.png" alt="Image" class="img-fluid">
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="300">
+                    <img src="images/logo_2.png" alt="Image" class="img-fluid">
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="400">
+                    <img src="images/logo_3.png" alt="Image" class="img-fluid">
+                </div>
+
+                <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="500">
+                    <img src="images/logo_4.png" alt="Image" class="img-fluid">
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="600">
+                    <img src="images/logo_5.png" alt="Image" class="img-fluid">
+                </div>
+                <!--
+                <div class="col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="700">
+                  <img src="images/logo_6.png" alt="Image" class="img-fluid">
+                </div>
+              -->
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <footer class="site-footer">
+        <?php include("footer.php"); ?>
+    </footer>
+
+</div>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/bootstrap-datepicker.min.js"></script>
+<script src="js/aos.js"></script>
+
+<script src="js/main.js"></script>
+
+</body>
+</html>
---
+++ web/BackUp_S2/js/contact.js
@@ -0,0 +1,37 @@
+//ENVOI DU FORMULAIRE
+$(document).ready(function() {
+$('#submitContact').click(function (e) {
+e.preventDefault();
+
+
+var objet = $('#objet').val();
+var message = $('#message').val();
+var email = $('#email').val();
+var envoi = $('#submitContact').val();
+
+$.post(
+'contact.php',
+{
+votremail:email,
+objet:objet,
+message:message,
+envoi:envoi
+},
+function (data) {
+console.log(data);
+if(data.includes("bien")){
+$('#errorContact').html(data).css("color","lightgreen");
+}
+else{
+$('#errorContact').html(data).css("color","lightcoral");
+}
+
+$('#objet').val("");
+$('#message').val("");
+$('#email').val("");
+}
+)
+
+
+});
+});
