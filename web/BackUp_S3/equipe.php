305b65176a94c8585cf3a97f11e5de2ccad0cc2d
GauthB <gauthier.bohyn@gmail.com>
2019-10-04 10:55:50 +0200

    backup S3

---
+++ web/BackUp_S3/equipe.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PeopleFlux Equipe</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="site-wrap">
+
+    <div class="site-mobile-menu">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+    </div>
+
+    <header class="site-navbar py-3" role="banner">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-11 col-xl-2">
+                    <h1 class="mb-0"><a href="index.html" class="text-white h2 mb-0">People<span class="text-primary">Flux</span> </a></h1>
+                </div>
+                <div class="col-12 col-md-10 d-none d-xl-block">
+                    <nav class="site-navigation position-relative text-right" role="navigation">
+                        <ul class="site-menu js-clone-nav mx-auto d-none d-lg-block">
+                            <li><a href="index.php">Accueil</a></li>
+                            <li><a href="info.php">A propos</a></li>
+                            <li class="active"><a href="equipe.php">Groupe</a></li>
+                            <li><a href="lieux.php">Lieux</a></li>
+                            <li><a href="contact.html">Contacts</a></li>
+                            <li class="cta"><a href="buy.php">Commander</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
+            </div>
+        </div>
+</div>
+</header>
+
+<div class="site-section site-hero inner">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-10">
+                <span class="d-block mb-3 caption" data-aos="fade-up">Nos membres</span>
+                <h1 class="d-block mb-4" data-aos="fade-up" data-aos-delay="100">Equipe</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="site-section">
+    <div class="container">
+
+        <!-- ################## Gauthier Bohyn ################## -->
+        <div class="row align-items-center speaker">
+            <div class="col-lg-6 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+                <img src="images/person_1.jpg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-lg-6 ml-auto">
+                <h2 class="text-white mb-4 name" data-aos="fade-right" data-aos-delay="200">Gauthier BOHYN</h2>
+                <div class="bio pl-lg-5">
+                    <span class="text-uppercase text-primary d-block mb-3" data-aos="fade-right" data-aos-delay="300">Producter Owner</span>
+                    <p class="mb-4" data-aos="fade-right" data-aos-delay="400">Explication perso...</p>
+                    <p data-aos="fade-right" data-aos-delay="500">
+                        <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                        <a href="#" class="p-2"><span class="icon-github"></span></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- ################## Aymeric Ponjee ################## -->
+        <div class="row align-items-center speaker">
+            <div class="col-lg-6 mb-5 mb-lg-0 order-lg-2" data-aos="fade" data-aos-delay="100">
+                <img src="images/person_2.jpg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-lg-6 ml-auto order-lg-1">
+                <h2 class="text-white mb-4 name" data-aos="fade-left" data-aos-delay="200">Aymeric PONJEE</h2>
+                <div class="bio pr-lg-5">
+                    <span class="text-uppercase text-primary d-block mb-3" data-aos="fade-left" data-aos-delay="300">Scrum Master</span>
+                    <p class="mb-4" data-aos="fade-left" data-aos-delay="400">Explication perso...</p>
+                    <p data-aos="fade-left" data-aos-delay="500">
+                        <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                        <a href="#" class="p-2"><span class="icon-github"></span></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- ################## Thibaut HERMANT ################## -->
+        <div class="row align-items-center speaker">
+            <div class="col-lg-6 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+                <img src="images/person_3.jpg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-lg-6 ml-auto">
+                <h2 class="text-white mb-4 name" data-aos="fade-right" data-aos-delay="200">Thibaut HERMANT</h2>
+                <div class="bio pl-lg-5">
+                    <span class="text-uppercase text-primary d-block mb-3" data-aos="fade-right" data-aos-delay="300">Membre</span>
+                    <p class="mb-4" data-aos="fade-right" data-aos-delay="400">Explication perso...</p>
+                    <p data-aos="fade-right" data-aos-delay="500">
+                        <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                        <a href="#" class="p-2"><span class="icon-github"></span></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- ################## Robin PAQUET ################## -->
+        <div class="row align-items-center speaker">
+            <div class="col-lg-6 mb-5 mb-lg-0 order-lg-2" data-aos="fade" data-aos-delay="100">
+                <img src="images/person_4.jpg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-lg-6 ml-auto order-lg-1">
+                <h2 class="text-white mb-4 name" data-aos="fade-left" data-aos-delay="200">Robin PAQUET</h2>
+                <div class="bio pr-lg-5">
+                    <span class="text-uppercase text-primary d-block mb-3" data-aos="fade-left" data-aos-delay="300">Membre</span>
+                    <p class="mb-4" data-aos="fade-left" data-aos-delay="400">Explication perso...</p>
+                    <p data-aos="fade-left" data-aos-delay="500">
+                        <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                        <a href="#" class="p-2"><span class="icon-github"></span></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- ################## Ludo VAN DEN DORPE ################## -->
+        <div class="row align-items-center speaker">
+            <div class="col-lg-6 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+                <img src="images/person_5.jpg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-lg-6 ml-auto">
+                <h2 class="text-white mb-4 name" data-aos="fade-right" data-aos-delay="200">Ludo VAN DEN DORPE</h2>
+                <div class="bio pl-lg-5">
+                    <span class="text-uppercase text-primary d-block mb-3" data-aos="fade-right" data-aos-delay="300">Membre</span>
+                    <p class="mb-4" data-aos="fade-right" data-aos-delay="400">Explication perso...</p>
+                    <p data-aos="fade-right" data-aos-delay="500">
+                        <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                        <a href="#" class="p-2"><span class="icon-github"></span></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- ################## Tristan PESTIAUX ################## -->
+
+        <div class="row align-items-center speaker">
+            <div class="col-lg-6 mb-5 mb-lg-0 order-lg-2" data-aos="fade" data-aos-delay="100">
+                <img src="images/person_6.jpg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-lg-6 ml-auto order-lg-1">
+                <h2 class="text-white mb-4 name" data-aos="fade-left" data-aos-delay="200">Tristan PESTIAUX</h2>
+                <div class="bio pr-lg-5">
+                    <span class="text-uppercase text-primary d-block mb-3" data-aos="fade-left" data-aos-delay="300">Membre</span>
+                    <p class="mb-4" data-aos="fade-left" data-aos-delay="400">Explication perso...</p>
+                    <p data-aos="fade-left" data-aos-delay="500">
+                        <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                        <a href="#" class="p-2"><span class="icon-github"></span></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- ################## Brian HANQUET ################## -->
+        <div class="row align-items-center speaker">
+            <div class="col-lg-6 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+                <img src="images/person_7.jpg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-lg-6 ml-auto">
+                <h2 class="text-white mb-4 name" data-aos="fade-right" data-aos-delay="200">Brian HANQUET</h2>
+                <div class="bio pl-lg-5">
+                    <span class="text-uppercase text-primary d-block mb-3" data-aos="fade-right" data-aos-delay="300">Membre</span>
+                    <p class="mb-4" data-aos="fade-right" data-aos-delay="400">Explication perso...</p>
+                    <p data-aos="fade-right" data-aos-delay="500">
+                        <a href="#" class="p-2"><span class="icon-facebook"></span></a>
+                        <a href="#" class="p-2"><span class="icon-github"></span></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+</div>
+
+
+
+
+
+
+
+<footer class="site-footer">
+    <?php include("footer.php"); ?>
+</footer>
+
+</div>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/bootstrap-datepicker.min.js"></script>
+<script src="js/aos.js"></script>
+
+<script src="js/main.js"></script>
+
+</body>
+</html>
