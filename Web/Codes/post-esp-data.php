8f8225254851553bc60c5d99a93bba2470771224
Robin Paquet <44506208+robinp12@users.noreply.github.com>
2019-10-10 11:23:48 +0200

    Update post-esp-data.php

--- Web/Codes/post-esp-data.php
+++ Web/Codes/post-esp-data.php
@@ -1,29 +1,29 @@
 <?php
 
-$servername = "localhost";
+$servername = "91.216.107.248";
 
 // REPLACE with your Database name
-$dbname = "wecod1168786_2jauzk";
+$dbname = "gauth1148636_1fzru";
 // REPLACE with Database user
-$username = "wecod1168786_2jauzk@localhost";
+$username = "gauth1148636_1fzru";
 // REPLACE with Database user password
-$password = "guyjptvc2p";
+$password = "n7ttg6wdjq";
 
 // Keep this API Key value to be compatible with the ESP32 code provided in the project page.
 // If you change this value, the ESP32 sketch needs to match
 $api_key_value = "tPmAT5Ab3j7F9";
 
-$api_key= $sensor = $location = $value1 = $value2 = $value3 = "";
+$api_key= $id = $id_stage = $nbr_entree = $nbr_sortie = $nbr_actuel = $heure = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = test_input($_POST["api_key"]);
     if($api_key == $api_key_value) {
-        $Stage_name = test_input($_POST["Stage_name"]);
-        $Stage_latitude = test_input($_POST["Stage_latitude"]);
-        $Stage_longitude = test_input($_POST["Stage_longitude"]);
-        $nbrOfPeople = test_input($_POST["nbrOfPeople"]);
-        $MaxPeople = test_input($_POST["MaxPeople"]);
-        $Event_id = test_input($_POST["Event_id"]);
+        $id = test_input($_POST["id"]);
+        $id_stage = test_input($_POST["id_stage"]);
+        $nbr_entree = test_input($_POST["nbr_entree"]);
+        $nbr_sortie = test_input($_POST["nbr_sortie"]);
+        $nbr_actuel = test_input($_POST["nbr_actuel"]);
+        $heure = test_input($_POST["heure"]);
 
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
@@ -32,8 +32,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             die("Connection failed: " . $conn->connect_error);
         }
 
-        $sql = "INSERT INTO Stages (Stage_name, Stage_latitude, Stage_longitude, nbrOfPeople, MaxPeople, Event_id)
-        VALUES ('" . $Stage_name . "', '" . $Stage_latitude . "', '" . $Stage_longitude . "', '" . $nbrOfPeople . "', '" . $MaxPeople . "', '" . $Event_id . "')";
+        $sql = "INSERT INTO Nbr_Personne (id_stage, nbr_entree, nbr_sortie, nbr_actuel, heure)
+        VALUES ('" . $id . "', '" . $id_stage . "', '" . $nbr_entree . "', '" . $nbr_sortie . "', '" . $nbr_actuel . "', '" . $heure . "')";
 
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
