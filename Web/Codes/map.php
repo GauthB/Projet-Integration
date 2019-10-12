62eacb556438e0e861d186426517814c14d5ba8b
Hanquet Brian <he201459@students.ephec.be>
2019-10-12 13:21:43 +0200

    Update new Files

--- Web/Codes/map.php
+++ Web/Codes/map.php
@@ -1,43 +1,32 @@
+
 <?php
-$servername = "91.216.107.248";
-$username = "wecod1168786_2jauzk";
-$password = "guyjptvc2p";
-$dbname = "wecod1168786_2jauzk";
-
-
-try {
-    $conn = new PDO('mysql:host='.$servername.';dbname='.$dbname, $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-}
-catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage();
-}
+/*
+require_once "db_connect.php";
+$eventNameQuery = $dbh->query('SELECT id_event, event_name FROM Events ORDER BY event_name');
+$eventNames = $eventNameQuery->fetchAll(PDO::FETCH_ASSOC);
+*/
 ?>
+
 
 <div class="container">
 
     <link rel="stylesheet" href="css/add.css">
     <ul>
-        <li>
-            <input classe="btnMap" type="button" value="24h Vélo" id="24hvelo" > <!--onclick="javascript:afficher_cacher('meteovelo');"-->
-        </li>
-        <li>
-            <input classe="btnMap" type="button" value="Welcome Spring Festival" id="wsp" >
-        </li>
-       <li>
-            <input classe="btnMap" type="button" value="Ephec de Louvain-la-Neuve" id="ephec" >
-        </li>
-        <!--
-        <li>
-            <input type="button" value="Ephec" id="ephec" >
-        </li>
-        <li>
-            <input type="button" value="Clear" id="clear">
-        </li>-->
+        <?php
+
+        foreach ($eventNames as $eventName) {
+            echo '<li><input classe="btnMap" type="button" value="' . $eventName['event_name'] . '" id="btn' . $eventName['id_event'] . '"></li>';
+        }
+        ?>
+        <li><input classe="btnMap" type="button" value="24 Heures vélo" id="24hvelo"></li>
+        <li><input classe="btnMap" type="button" value="Welcome Spring! Festival 2019" id="wsp"></li>
     </ul>
+
+    <?php foreach ($eventNames as $eventName): ?>
+        <div id="title<?=$eventName['id_event']?>" style='display:none;'>
+            <span class="d-block mb-3 caption" data-aos="fade-up" style="text-align: center;text-decoration: underline;"><i><?=$eventName['event_name']?></i></span>
+        </div>
+    <?php endforeach;?>
 
     <div id="titleVelo" class="" style='display:none;'>
         <span class="d-block mb-3 caption" data-aos="fade-up" style="text-align: center;text-decoration: underline;"><i>24H VELO</i></span>
@@ -45,9 +34,7 @@ catch(PDOException $e)
     <div id="titleWsp" class="" style='display:none;'>
         <span class="d-block mb-3 caption" data-aos="fade-up" style="text-align: center;text-decoration: underline;"><i>WELCOME SPRING FESTIVAL</i></span>
     </div>
-    <div id="titleEphec" class="" style='display:none;'>
-        <span class="d-block mb-3 caption" data-aos="fade-up" style="text-align: center;text-decoration: underline;"><i>EPHEC LLN</i></span>
-    </div>
+
     <div id="mapid" style=" height: 180px; height: 480px; "></div>
     <script>
 
@@ -75,7 +62,7 @@ catch(PDOException $e)
         var streetA1Participant = 0;
         var streetA1ParticipantMax = 10;
 
-        var streetA2Participant = 10;
+        var streetA2Participant = 0;
         var streetA2ParticipantMax = 20;
 
         //Variables nécessaire | A pour le premier client |  1-2 pour les coordonnées soumises
@@ -219,73 +206,6 @@ catch(PDOException $e)
 
         };
 
-        //#################################################################################################################################################################
-        //#################################  Evenement C - Ephec de LLN  ##################################################################################################
-        //#################################################################################################################################################################
-
-
-
-        var coordonneeC1 = [50.665702, 4.612218];
-
-        var streetC1Participant = 3;
-
-        var streetC1;
-        var coordonnesC11;
-
-
-        var hvelo = document.getElementById('ephec');               //bug dans le bouton. Fonctionne bien mais pas parfaitement pour retirer
-        hvelo.onclick = function() {
-
-
-
-
-            if (coordonnesC11==coordonneeC1[0]){
-
-                streetC1 = streetC1.remove();
-                coordonnesC11=0;
-                document.getElementById('titleEphec').style.display = 'none';
-
-
-            }
-            else{
-
-
-                coordonnesC11=coordonneeC1[0];
-                streetC1 = L.marker([coordonneeC1[0],coordonneeC1[1]]).addTo(mymap);
-                streetC1.bindPopup("<b>Parking Leclercq</b><br>Il y a "+streetC1Participant+" participant(s)!<br> Le nombre maximum de participant est estimé à " + streetC1ParticipantMax);
-
-
-
-                document.getElementById('titleEphec').style.display = 'block';
-
-
-
-
-            }
-
-            document.getElementById('meteoA').style.display = 'none';
-            document.getElementById('titleVelo').style.display = 'none';
-            coordonnesA11=0;
-            streetA1.remove();
-            streetA2.remove();
-            polygonA1.remove();
-
-            document.getElementById('titleWsp').style.display = 'none';
-            document.getElementById('meteoB').style.display = 'none';
-            streetB1.remove();
-            polygonB1.remove();
-            coordonnesB11=0;
-
-
-
-        };
-
-
-
-        //50.665660, 4.612205
-
-
-
 
     </script>
     <!-- https://www.youtube.com/watch?v=g8bV6559bIo Pour la map-->
@@ -301,20 +221,5 @@ catch(PDOException $e)
     <div id="meteoB" style='display:none;' >
         <div id="m-booked-weather-bl250-71420"> <div class="booked-wzs-250-175 weather-customize" style="background-color:#137AE9;width:259px;" id="width1"> <div class="booked-wzs-250-175_in"> <div class="booked-wzs-250-175-data"> <div class="booked-wzs-250-175-left-img wrz-18"> <a target="_blank" href="https://www.booked.net/"> <img src="//s.bookcdn.com/images/letter/logo.gif" alt="booked.net" /> </a> </div> <div class="booked-wzs-250-175-right"> <div class="booked-wzs-day-deck"> <div class="booked-wzs-day-val"> <div class="booked-wzs-day-number"><span class="plus">+</span>18</div> <div class="booked-wzs-day-dergee"> <div class="booked-wzs-day-dergee-val">&deg;</div> <div class="booked-wzs-day-dergee-name">C</div> </div> </div> <div class="booked-wzs-day"> <div class="booked-wzs-day-d">H: <span class="plus">+</span>18&deg;</div> <div class="booked-wzs-day-n">L: <span class="plus">+</span>13&deg;</div> </div> </div> <div class="booked-wzs-250-175-info"> <div class="booked-wzs-250-175-city smolest">Louvain-la-Neuve </div> <div class="booked-wzs-250-175-date">Mardi, 01 Octobre</div> <div class="booked-wzs-left"> <span class="booked-wzs-bottom-l">Prévisions sur 7 jours</span> </div> </div> </div> </div> <a target="_blank" href="https://hotelmix.fr/weather/louvain-la-neuve-30446"> <table cellpadding="0" cellspacing="0" class="booked-wzs-table-250"> <tr> <td>Mer.</td> <td>Jeu.</td> <td>Ven.</td> <td>Sam.</td> <td>Dim.</td> <td>Lun.</td> </tr> <tr> <td class="week-day-ico"><div class="wrz-sml wrzs-18"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-03"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-18"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-03"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-18"></div></td> <td class="week-day-ico"><div class="wrz-sml wrzs-03"></div></td> </tr> <tr> <td class="week-day-val"><span class="plus">+</span>14&deg;</td> <td class="week-day-val"><span class="plus">+</span>14&deg;</td> <td class="week-day-val"><span class="plus">+</span>16&deg;</td> <td class="week-day-val"><span class="plus">+</span>18&deg;</td> <td class="week-day-val"><span class="plus">+</span>18&deg;</td> <td class="week-day-val"><span class="plus">+</span>16&deg;</td> </tr> <tr> <td class="week-day-val"><span class="plus">+</span>9&deg;</td> <td class="week-day-val"><span class="plus">+</span>7&deg;</td> <td class="week-day-val"><span class="plus">+</span>11&deg;</td> <td class="week-day-val"><span class="plus">+</span>13&deg;</td> <td class="week-day-val"><span class="plus">+</span>10&deg;</td> <td class="week-day-val"><span class="plus">+</span>9&deg;</td> </tr> </table> </a> </div></div> </div><script type="text/javascript"> var css_file=document.createElement("link"); css_file.setAttribute("rel","stylesheet"); css_file.setAttribute("type","text/css"); css_file.setAttribute("href",'https://s.bookcdn.com/css/w/booked-wzs-widget-275.css?v=0.0.1'); document.getElementsByTagName("head")[0].appendChild(css_file); function setWidgetData(data) { if(typeof(data) != 'undefined' && data.results.length > 0) { for(var i = 0; i < data.results.length; ++i) { var objMainBlock = document.getElementById('m-booked-weather-bl250-71420'); if(objMainBlock !== null) { var copyBlock = document.getElementById('m-bookew-weather-copy-'+data.results[i].widget_type); objMainBlock.innerHTML = data.results[i].html_code; if(copyBlock !== null) objMainBlock.appendChild(copyBlock); } } } else { alert('data=undefined||data.results is empty'); } } </script> <script type="text/javascript" charset="UTF-8" src="https://widgets.booked.net/weather/info?action=get_weather_info&ver=6&cityID=30446&type=3&scode=124&ltid=3458&domid=581&anc_id=22882&cmetric=1&wlangID=3&color=137AE9&wwidth=259&header_color=ffffff&text_color=333333&link_color=08488D&border_form=1&footer_color=ffffff&footer_text_color=333333&transparent=0"></script><!-- weather widget end -->
     </div>
-
-
-
-
-
-
-
-
-
-
-    <script>
-
-
-
-    </script>
 
 </div>
