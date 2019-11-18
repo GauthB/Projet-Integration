cc44e19b2d09bdde493d819ae25072e16d59efb2
GauthB <gauthier.bohyn@gmail.com>
2019-11-18 23:04:15 +0100

    supprimer dossier clockify

--- Web/Codes/__test__/test1.js
+++ Web/Codes/__test__/test1.js
@@ -3,15 +3,16 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('https://peopleflux.wecodx.com/contact.html');
+    await driver.get('https://peopleflux.gauthierbohyn.com/pageContact.php');
+    await driver.sleep(2000);
     await driver.findElement(By.id('email')).sendKeys('test@gmail.com', Key.TAB);
-    await driver.sleep(1500);
+    await driver.sleep(2000);
     await driver.findElement(By.id('objet')).sendKeys('Test Interface', Key.TAB);
-    await driver.sleep(1500);
+    await driver.sleep(2000);
     await driver.findElement(By.id('message')).sendKeys('Ceci est un test d interface', Key.TAB);
-    await driver.sleep(1500);
-    //await driver.findElement(By.id('submitContact')).sendKeys(Key.ENTER);
-    //await driver.sleep(500);
+    await driver.sleep(2000);
+   // await driver.findElement(By.id('submitContact')).sendKeys(Key.ENTER);
+   // await driver.sleep(2000);
 
   } finally {
     await driver.quit();
