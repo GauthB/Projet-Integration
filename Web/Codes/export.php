6d12efc099e06095b8a2f7db48ac179e1aafef3a
Aymeric Ponjée <36765184+Tryggon@users.noreply.github.com>
2019-11-25 15:58:43 +0100

    Update export.php

--- Web/Codes/export.php
+++ Web/Codes/export.php
@@ -6,4 +6,10 @@ if(!isset($_SESSION['id'])) {
 }
 require_once "db_connect.php";
 require_once "esp-data.php";
-?>"Nom de scène";"ID";"Entrées";"Sorties";"Actuel";"Heure"
+
+$req = $dbh->prepare('SELECT stage_name, id_stage, nbr_entree, nbr_sortie, nbr_actuel, heure FROM evenements');
+$req->execute();
+$data = $req->fetchAll();
+require 'class_csv.php';
+CSV::export($data,'stat_event');
+?>
