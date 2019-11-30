ccaa4f19279fa38f7a1b991b2f41d1ba11af96f6
Tristan Pestiaux <45734363+abyssin14@users.noreply.github.com>
2019-11-30 13:43:33 +0100

    update launch screen

--- App/Codes/PeopleFlux/App.js
+++ App/Codes/PeopleFlux/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navigation from './Navigation/Navigation'
 import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
-import {StatusBar } from 'react-native'
+import {StatusBar, View, StyleSheet } from 'react-native'
 import SplashScreen from 'react-native-splash-screen';
 
 
@@ -12,10 +12,19 @@ export default class App extends React.Component {
 }
   render() {
     return (
-      <Provider store={Store}>
-        <StatusBar backgroundColor="#232531" barStyle="light-content"/>
-          <Navigation/>
-      </Provider>
+        <Provider store={Store}>
+          <View style={styles.main_container}>
+            <StatusBar backgroundColor="#232531" barStyle="light-content"/>
+            <Navigation/>
+            </View>
+        </Provider>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    backgroundColor: '#232531'
+  }
+})
