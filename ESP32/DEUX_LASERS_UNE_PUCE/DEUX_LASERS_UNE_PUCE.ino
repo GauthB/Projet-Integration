ea2e14b62263812e6bc1445ba9dd5c324b5a5ff6
Robin Paquet <44506208+robinp12@users.noreply.github.com>
2019-12-02 22:42:14 +0100

    Correction code 2 lasers

--- ESP32/DEUX_LASERS_UNE_PUCE/DEUX_LASERS_UNE_PUCE.ino
+++ ESP32/DEUX_LASERS_UNE_PUCE/DEUX_LASERS_UNE_PUCE.ino
@@ -1,71 +1,65 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
 /*
   2 LASERS SUR UNE SEULE CARTE POUR COMPTER 
   LES ENTREES/SORTIES DANS UNE SEULE PORTE
 */
+
 boolean compt1 = 0;
 boolean compt2 = 0;// VALEUR PRECEDENTE DU LASER AU COMPTEUR
-boolean entre = 0;
-boolean sort = 0;
 
 void setup(){                
-  Serial.begin(9600);
-  pinMode(13, OUTPUT); // EMETTEUR LASER 1
-  pinMode(12, INPUT);  // RECEPTEUR LASER 1
-  pinMode(26, OUTPUT); // EMETTEUR LASER 2
-  pinMode(25, INPUT);  // RECEPTEUR LASER 2
-  pinMode(27, OUTPUT); // LED 
-  pinMode(33, OUTPUT); // Bouton retour a zero
-  pinMode(32, INPUT);  // Bouton retour a zero
-  Serial.println("Debut du programme");
+  Serial.begin(115200);
+  pinMode(19, INPUT);  // RECEPTEUR LASER 1
+  pinMode(23, INPUT);  // RECEPTEUR LASER 2
+  pinMode(21, OUTPUT); // LED bleu
+  pinMode(18, OUTPUT); // LED rouge
 }
-int laser1(){
-  int valeur = (digitalRead(12)); // VALEUR RECEPTEUR LASER
-  digitalWrite(13, HIGH); // EMETTEUR LASER
-  if(valeur==1 && compt1 == 1){
-    compt1 = 0;
-    digitalWrite(27, HIGH);
+void ledBlink(int x, int y){
+  int i;
+  for(i=0;i<x;i++){
+    digitalWrite(y, HIGH);
     delay(200);
-    digitalWrite(27, LOW);
-    return 1;
+    digitalWrite(y, LOW);
+    delay(100);
   }
-  else if(valeur == 0){
-    compt1 = 1;
-    delay(300);
-    return 0;
-  }
+}
+
+int laser1(){ // Retourne 1 quand le laser est coupé
+  int valeur = (digitalRead(19)); // VALEUR RECEPTEUR LASER
+    if(valeur==0 && compt1 == 1){
+      compt1 = 0;  
+        if(laser2() < laser1()){
+          ledBlink(1,18);
+          Serial.println("Entre");
+          delay(1000);
+        }   
+      return 1;
+    }
+    if(valeur == 1){
+      compt1 = 1;
+      return 0;
+    }
 }
 int laser2(){
-  int valeur = (digitalRead(25)); // VALEUR RECEPTEUR LASER
-  digitalWrite(26, HIGH); // EMETTEUR LASER
-  if(valeur == 1 && compt2 == 1){
-    compt2 = 0;
-    digitalWrite(27, HIGH);
-    delay(200);
-    digitalWrite(27, LOW);
-    return 1;
-  }
-  else if(valeur == 0){
-    compt2 = 1;
-    delay(300);
-    return 0;
-  }
+  int valeur = (digitalRead(23)); // VALEUR RECEPTEUR LASER
+    if(valeur == 0 && compt2 == 1){
+      compt2 = 0;
+        if(laser1() < laser2()){
+          ledBlink(1,21);
+          Serial.println("Sort");
+          delay(1000);
+        }
+      return 1;
+    }
+    if(valeur == 1){
+      compt2 = 1;
+      return 0;
+    }
 }
-void passage(){
-  if(laser1() == 1 && laser2() == 0){
-    entre = 1;
-  }
-  if(laser2() == 1 && entre == 1){
-    Serial.println("Entre");
-    entre = 0;
-  }
-  if(laser2() == 1 && laser1() == 0){
-    sort = 1;
-  }
-  if(laser1() == 1 && sort == 1){
-    Serial.println("Sort");
-    sort = 0;
-  }
-}
+
 void loop(){
-  passage();  
+  laser1();
+  laser2();
 }
