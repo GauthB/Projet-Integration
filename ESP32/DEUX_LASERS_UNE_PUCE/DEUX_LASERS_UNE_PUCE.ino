10479bd030ba15894b0f1f400d58c0a6dd58cfd7
Robin Paquet <44506208+robinp12@users.noreply.github.com>
2019-12-03 15:54:55 +0100

    Modif + Commentaire code esp 2 lasers
    Amelioration du code

--- ESP32/DEUX_LASERS_UNE_PUCE/DEUX_LASERS_UNE_PUCE.ino
+++ ESP32/DEUX_LASERS_UNE_PUCE/DEUX_LASERS_UNE_PUCE.ino
@@ -6,60 +6,60 @@
   LES ENTREES/SORTIES DANS UNE SEULE PORTE
 */
 
-boolean compt1 = 0;
-boolean compt2 = 0;// VALEUR PRECEDENTE DU LASER AU COMPTEUR
+boolean prev1 = 0; // Valeur précédente du recepteur 1
+boolean prev2 = 0; // Valeur précédente du recepteur 2
 
 void setup(){                
-  Serial.begin(115200);
-  pinMode(19, INPUT);  // RECEPTEUR LASER 1
-  pinMode(23, INPUT);  // RECEPTEUR LASER 2
-  pinMode(21, OUTPUT); // LED bleu
-  pinMode(18, OUTPUT); // LED rouge
+  Serial.begin(115200); // Vitesse du taux de donnée
+  pinMode(19, INPUT);  // Recepteur 1 en mode input
+  pinMode(23, INPUT);  // Recepteur 2 en mode input
+  pinMode(21, OUTPUT); // LED bleu en mode output
+  pinMode(18, OUTPUT); // LED rouge en mode output
 }
-void ledBlink(int x, int y){
+void ledBlink(int x, int y){ // Clignotement LED
   int i;
-  for(i=0;i<x;i++){
-    digitalWrite(y, HIGH);
-    delay(200);
-    digitalWrite(y, LOW);
-    delay(100);
+  for(i=0;i<x;i++){       
+    digitalWrite(y, HIGH); // Allume led
+    delay(200);            // Attend 0.2s
+    digitalWrite(y, LOW);  // Eteint led
+    delay(100);            // Attend 0.1s
   }
 }
 
-int laser1(){ // Retourne 1 quand le laser est coupé
-  int valeur = (digitalRead(19)); // VALEUR RECEPTEUR LASER
-    if(valeur==0 && compt1 == 1){
-      compt1 = 0;  
-        if(laser2() < laser1()){
-          ledBlink(1,18);
+int laser1(){                     // Retourne 1 quand le laser est coupé
+  int valeur = (digitalRead(19)); // Valeur recue au recepteur 1
+    if(valeur==0 && prev1==1){  // Verifie que le laser soit coupé
+      prev1 = 0;  
+        if(laser2() < laser1() && laser2()== 0){ // Si le laser 1 est coupé avant le laser 2
+          ledBlink(1,21);        // LED clignote
           Serial.println("Entre");
-          delay(1000);
+          delay(1500);           // On attend 1.5s 
+          return 1;              // On renvoit 1 pour dire qu'une personne entre
         }   
-      return 1;
     }
-    if(valeur == 1){
-      compt1 = 1;
-      return 0;
+    if(valeur==1){               // Le laser n'est pas coupé
+      prev1 = 1;                 // On remet la var precedente a 1 pour ne pas rentrer dans la premiere condition
+      return 0;                  // On renvoit 0 pour dire que personne ne passe
     }
 }
-int laser2(){
-  int valeur = (digitalRead(23)); // VALEUR RECEPTEUR LASER
-    if(valeur == 0 && compt2 == 1){
-      compt2 = 0;
-        if(laser1() < laser2()){
-          ledBlink(1,21);
+int laser2(){                     // Retourne 1 quand le laser est coupé
+  int valeur = (digitalRead(23)); // Valeur recue au recepteur 2
+    if(valeur==0 && prev2==1){  // Verifie que le laser soit coupé
+      prev2 = 0;
+        if(laser1() < laser2()  && laser1()== 0){ // Si le laser 2 est coupé avant le laser 1
+          ledBlink(1,18);        // LED clignote
           Serial.println("Sort");
-          delay(1000);
+          delay(1500);           // On attend 1.5s
+          return 1;              // On renvoit 1 pour dire qu'une personne entre
         }
-      return 1;
     }
-    if(valeur == 1){
-      compt2 = 1;
-      return 0;
+    if(valeur==1){               // Le laser n'est pas coupé
+      prev2 = 1;                 // On remet la var precedente a 1 pour ne pas rentrer dans la premiere condition
+      return 0;                  // On renvoit 0 pour dire que personne ne passe
     }
 }
 
 void loop(){
-  laser1();
-  laser2();
+  laser1();     // On appelle la fonction pour le premier laser
+  laser2();     // On appelle la fonction pour le deuxieme laser
 }
