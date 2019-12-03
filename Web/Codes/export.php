877fdc1134d30ba7fbff544bcb7b94f3e6561c19
Aymeric Ponjée <36765184+Tryggon@users.noreply.github.com>
2019-12-03 19:00:43 +0100

    Update export.php

--- Web/Codes/export.php
+++ Web/Codes/export.php
@@ -2,15 +2,14 @@
 header('Content-Type: text/csv');
 header('Content-Disposition: attachement; filename="stats.csv"');
 
-try{
-    $PDO = new PDO("91.216.107.248", "gauth1148636_1fzru", "n7ttg6wdjq");
-    $PDO->setAttribute(PDO::ATR_ERRMODE,PDO::ERRMODE_WARNING);
-    $PDO->setAttribute(PDO::ATR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-}catch(PDOException $e){
-    echo 'Connexion impossible';
-}
-$req = $PDO->prepare('SELECT stage_name, nbr_entree, nbr_sortie, heure FROM nbr_Personne ');
-$req->execute();
-$data = $req->fetchAll();
-print_r($data); exit();
-?>
+require_once "php/db_connect.php";
+
+$eventInfoQuery = $dbh->query('SELECT stage_name, nbr_entree, nbr_sortie, heure 
+                               FROM Nbr_Personne 
+                               join Stages on Stages.id_stage = Nbr_Personne.id_stage');
+
+$eventInfos = $eventInfoQuery->fetchAll(PDO::FETCH_ASSOC);
+?>"Nom de la scene";"Nombre entrees";"Nombre sorties";"Heure"<?php
+foreach($eventInfos as $e){
+    echo "\n".'"'.$e->stage_name.'";"'.$e->nbr_entree.'";"'.$e->nbr_sortie.'";"'.$e->heure.'"';
+}?>
