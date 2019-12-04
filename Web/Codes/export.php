73d46631a34b38c7c366d620830273be096bf267
Aymeric Ponjée <36765184+Tryggon@users.noreply.github.com>
2019-12-04 14:56:34 +0100

    Excel

--- Web/Codes/export.php
+++ Web/Codes/export.php
@@ -1,6 +1,6 @@
 <?php
-header('Content-Type: text/csv');
-header('Content-Disposition: attachement; filename="stats.csv"');
+//header('Content-Type: text/csv');
+//header('Content-Disposition: attachement; filename="stats.csv"');
 
 require_once "php/db_connect.php";
 
@@ -9,7 +9,13 @@ $eventInfoQuery = $dbh->query('SELECT stage_name, nbr_entree, nbr_sortie, heure
                                join Stages on Stages.id_stage = Nbr_Personne.id_stage');
 
 $eventInfos = $eventInfoQuery->fetchAll(PDO::FETCH_ASSOC);
-?>"Nom de la scene";"Nombre entrees";"Nombre sorties";"Heure"<?php
-foreach($eventInfos as $e){
-    echo "\n".'"'.$e->stage_name.'";"'.$e->nbr_entree.'";"'.$e->nbr_sortie.'";"'.$e->heure.'"';
-}?>
+$data = array();
+foreach ($eventInfos as $d){
+    $data[] = array(
+        'Nom du stage' => $d->stage_name,
+        'Nombre entree' => $d->nbr_entree,
+        'Nombre entree' => $d->nbr_sortie,
+        'heure'      => $d->heure
+    );
+}
+print_r($data);
