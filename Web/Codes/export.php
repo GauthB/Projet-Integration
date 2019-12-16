63cdbd4f2feaf46b447f17ae5824040593190ddc
Aymeric Ponjée <36765184+Tryggon@users.noreply.github.com>
2019-12-16 18:26:48 +0100

    Update export.php

--- Web/Codes/export.php
+++ Web/Codes/export.php
@@ -4,18 +4,33 @@
 
 require_once "php/db_connect.php";
 
-$eventInfoQuery = $dbh->query('SELECT stage_name, nbr_entree, nbr_sortie, heure 
-                               FROM Nbr_Personne 
-                               join Stages on Stages.id_stage = Nbr_Personne.id_stage');
+
+/*
+$sth = $dbh -> prepare('SELECT * FROM Stages JOIN Events ON Stages.id_event = Events.id_event WHERE id_client=:client_id');
+$sth->execute(array(':client_id' => $_SESSION['id']));
+$stageInfo = $sth -> fetchAll(PDO::FETCH_ASSOC);
+$sth->closeCursor();
+
+foreach ($stageInfo as $stage) {
+    $data[] = array(
+        $stage["event_name"] . '</td><td>' .
+        $stage["stage_name"] . '</td><td>' .
+        $stage["stage_latitude"] . '</td><td>' .
+        $stage["stage_longitude"] . '</td><td>' .
+        $stage["max_people"] . '</td><td>' .
+        $stage["hour_from"] . '</td><td>' .
+        $stage["hour_to"] . '</td><td>');
+        echo "'<span class=\"close btnDelStage\" data-idStage=\"' . $stage[id_stage] . '\"><i class=\"fas fa-backspace suppr\" style=\"margin-right:2.5rem\"></i></span></td></tr>'";
+}
+*/
+
+$eventInfoQuery = $dbh->query('SELECT stage_name as NOM, nbr_entree as ENTREE, nbr_sortie as SORTIE, heure
+                               FROM Nbr_Personne
+                               JOIN Stages on Stages.id_stage = Nbr_Personne.id_stage
+                               WHERE NOM');
 
 $eventInfos = $eventInfoQuery->fetchAll(PDO::FETCH_ASSOC);
-$data = array();
-foreach ($eventInfos as $d){
-    $data[] = array(
-        'Nom du stage' => $d->stage_name,
-        'Nombre entree' => $d->nbr_entree,
-        'Nombre entree' => $d->nbr_sortie,
-        'heure'      => $d->heure
-    );
-}
-print_r($data);
+
+require 'class_csv.php';
+CSV::export($eventInfos,'Export');
+?>
