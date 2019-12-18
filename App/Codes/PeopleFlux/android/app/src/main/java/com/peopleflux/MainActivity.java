1f7f83aacff54b8297bba875442a123bbc13ac85
Tristan Pestiaux <45734363+abyssin14@users.noreply.github.com>
2019-12-18 12:48:30 +0100

    create apk

--- App/Codes/PeopleFlux/android/app/src/main/java/com/peopleflux/MainActivity.java
+++ App/Codes/PeopleFlux/android/app/src/main/java/com/peopleflux/MainActivity.java
@@ -6,16 +6,16 @@ import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
+  @Override
+protected void onCreate(Bundle savedInstanceState) {
+   SplashScreen.show(this);  // here
+   super.onCreate(savedInstanceState);
+}
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-   @Override
-protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);  // here
-    super.onCreate(savedInstanceState);
-}
-
   @Override
   protected String getMainComponentName() {
     return "PeopleFlux";
--- App/Codes/PeopleFlux/android/app/src/main/java/com/peopleflux/MainApplication.java
+++ App/Codes/PeopleFlux/android/app/src/main/java/com/peopleflux/MainApplication.java
@@ -7,9 +7,9 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.mapbox.rctmgl.RCTMGLPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.mapbox.rctmgl.RCTMGLPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
